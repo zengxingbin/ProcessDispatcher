ea17d3257a4eb8a7380df1bb088400d7b09e5e84
zengxingbin <2045620125@qq.com>
2017-09-05 11:47:54 +0800

    a new project

---
+++ src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
+public class Test {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/util/PriorityComparator.java
@@ -0,0 +1,14 @@
+package util;
+
+import java.util.Comparator;
+
+import application.ProcessPCB;
+
+public class PriorityComparator implements Comparator<ProcessPCB> {
+
+    @Override
+    public int compare(ProcessPCB pro1, ProcessPCB pro2) {
+        return pro1.getPriority() - pro2.getPriority();
+    }
+
+}
